c3f5b426ad1425634ed5eb40bccd5fa6f4dabfd1
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-08-25 00:47:54 +0200

    Add a wrapper for Team ids
    We can implement team specific functionality here if needed. At the
    moment its only a wrapper for the team id.
    
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

---
+++ libmue/teams.h
@@ -0,0 +1,45 @@
+#ifndef MUE__TEAMS_H
+#define MUE__TEAMS_H
+
+
+#include <iostream>
+
+#include "config.h"
+
+
+namespace mue {
+
+
+class Team {
+	private:
+		Team_id _id;
+
+		Team() {}
+	public:
+		Team(Team_id id)
+		: _id(id)
+		{}
+
+		inline bool operator<(const Team& other)  const { return _id < other._id; }
+		inline bool operator>(const Team& other)  const { return _id > other._id; }
+		inline bool operator<=(const Team& other) const { return _id <= other._id; }
+		inline bool operator>=(const Team& other) const { return _id >= other._id; }
+		inline bool operator==(const Team& other) const { return _id == other._id; }
+		inline bool operator!=(const Team& other) const { return _id != other._id; }
+
+		inline bool operator<(const  Team_id other) const { return _id < other; }
+		inline bool operator>(const  Team_id other) const { return _id > other; }
+		inline bool operator<=(const Team_id other) const { return _id <= other; }
+		inline bool operator>=(const Team_id other) const { return _id >= other; }
+		inline bool operator==(const Team_id other) const { return _id == other; }
+		inline bool operator!=(const Team_id other) const { return _id != other; }
+
+		inline operator Team_id() const { return _id; }
+		inline Team_id id() const { return _id; }
+
+};
+
+std::ostream& operator<<(std::ostream& os, Team const& team) { return os << int(team.id()); }
+
+}
+#endif
