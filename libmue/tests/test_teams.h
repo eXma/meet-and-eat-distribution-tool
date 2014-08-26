ffa5a135f01dc472fc9b76bc0954c3748129ea0c
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-08-27 01:25:44 +0200

    Add teams tests
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

---
+++ libmue/tests/test_teams.h
@@ -0,0 +1,28 @@
+#ifndef TEST_TEAMS_H
+#define TEST_TEAMS_H
+
+#include <cxxtest/TestSuite.h>
+
+#include "teams.h"
+
+
+class TestTeamClass : public CxxTest::TestSuite
+{
+	public:
+		void testTramClass(void)
+		{
+			mue::Team t1(0);
+			mue::Team t2(1);
+			mue::Team t3(0);
+
+			TS_ASSERT(t1 < t2);
+			TS_ASSERT(t1 != t2);
+			TS_ASSERT(t1 == t3);
+			TS_ASSERT(mue::Team_id(t1) == 0);
+
+			TS_ASSERT_EQUALS(t1, 0);
+			TS_ASSERT_EQUALS(t1.id(), 0);
+		}
+};
+
+#endif
