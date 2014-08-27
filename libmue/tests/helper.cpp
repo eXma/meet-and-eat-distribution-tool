1def5d425f0d04833ab258268073de83ac6eed85
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-08-27 02:58:37 +0200

    Add a helper library to the libmue unittests
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

---
+++ libmue/tests/helper.cpp
@@ -0,0 +1,10 @@
+#include "helper.h"
+
+std::vector<mue::Team> test::make_testteams(int num)
+{
+	std::vector<mue::Team> teams;
+	
+	for (mue::Team_id i = 0; i < num; ++i)
+		teams.push_back(mue::Team(i));
+	return teams;
+}
---
+++ libmue/tests/helper.h
@@ -0,0 +1,12 @@
+#ifndef TESTS__HELPER_H
+#define TESTS__HELPER_H
+
+#include <vector>
+
+#include "teams.h"
+
+namespace test {
+	std::vector<mue::Team> make_testteams(int num);
+}
+
+#endif
