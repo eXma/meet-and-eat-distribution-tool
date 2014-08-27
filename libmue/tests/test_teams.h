76efd5db57f8379b079ead8181d076541a79701b
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-08-27 02:59:10 +0200

    more Teams unittests
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- libmue/tests/test_teams.h
+++ libmue/tests/test_teams.h
@@ -4,12 +4,13 @@
 #include <cxxtest/TestSuite.h>
 
 #include "teams.h"
+#include "helper.h"
 
 
 class TestTeamClass : public CxxTest::TestSuite
 {
 	public:
-		void testTramClass(void)
+		void testTeamClass(void)
 		{
 			mue::Team t1(0);
 			mue::Team t2(1);
@@ -22,6 +23,17 @@ class TestTeamClass : public CxxTest::TestSuite
 
 			TS_ASSERT_EQUALS(mue::Team_id(t1), 0);
 			TS_ASSERT_EQUALS(t1.id(), 0);
+		}
+
+		void testTeamGeneration(void)
+		{
+			std::vector<mue::Team> teams(std::move(test::make_testteams(9)));
+
+			TS_ASSERT_EQUALS(teams.size(), 9);
+
+			for (unsigned i = 0; i < teams.size(); ++i) {
+				TS_ASSERT(teams[i] == i);
+			}
 		}
 };
 
