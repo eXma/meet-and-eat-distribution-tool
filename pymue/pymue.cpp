55b0b3bee2d219b3dfa42fec7ca53497e6d08212
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-08-28 01:58:02 +0200

    Replace Seen_table with Seen_table_wrapper in the binding
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- pymue/pymue.cpp
+++ pymue/pymue.cpp
@@ -3,7 +3,9 @@
 #include "mue_algorithm.h"
 #include "teams.h"
 #include "distances.h"
-#include "seen_table.h"
+
+#include "seen_table_wrapper.h"
+
 
 
 char const * peng() {
@@ -24,9 +26,9 @@ BOOST_PYTHON_MODULE (_pymue)
 		.def("set_cost", &mue::Distance_matrix::set_cost)
 		.def("lookup", &mue::Distance_matrix::lookup);
 
-	class_<mue::Seen_table>("SeenTable", init<int>())
-		.def("clone", &mue::Seen_table::clone)
-		.def("generation", &mue::Seen_table::generation)
-		.def("seen", &mue::Seen_table::seen)
-		.def("add_meeting", &mue::Seen_table::add_meeting);
+	class_<pymue::Seen_table_wrapper>("SeenTable", init<int>())
+		.def("clone", &pymue::Seen_table_wrapper::clone)
+		.def("generation", &pymue::Seen_table_wrapper::generation)
+		.def("seen", &pymue::Seen_table_wrapper::seen)
+		.def("add_meeting", &pymue::Seen_table_wrapper::add_meeting);
 }
