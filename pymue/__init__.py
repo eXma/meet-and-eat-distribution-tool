607728b17c0a79725d997b458a53d1b3d1394a59
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-09-01 21:11:57 +0200

    Add string representation to GuestPair
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- pymue/__init__.py
+++ pymue/__init__.py
@@ -1,1 +1,6 @@
-from _pymue import peng, Team, DistanceMatrix, SeenTable, GuestTupleGenerator
+from _pymue import peng, Team, DistanceMatrix, SeenTable, GuestTupleGenerator, GuestPair
+
+def pair_pprint(pair):
+    return "(%s, %s)" % (pair.first, pair.second)
+
+GuestPair.__repr__ = pair_pprint
