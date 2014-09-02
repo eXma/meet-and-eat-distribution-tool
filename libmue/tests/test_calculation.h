e53962399112c0b0687bf73a8a721f3baf68f8d0
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-09-02 22:32:08 +0200

    Add a unittest for the Calculation of distances
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- libmue/tests/test_calculation.h
+++ libmue/tests/test_calculation.h
@@ -50,6 +50,30 @@ class TestCalculation : public CxxTest::TestSuite
 
 		}
 
+
+		void testGuestDistance(void)
+		{
+			mue::Distance_matrix matrix(9);
+			for (int k = 0; k < 2; ++k) {
+				int lower = k * 3;
+				int upper = lower + 3;
+				for (int i = lower; i < upper; ++i) {
+					for (int j = 3 + lower; j < 3 + upper; ++j) {
+						matrix.set_cost(i, j, k + 1);
+					}
+				}
+			}
+
+			mue::Calculation  calc(9, matrix);
+			mue::Calculation::Round_data round_data(mue::Calculation::SECOND,
+								std::vector<mue::Team_id>(),
+								std::vector<mue::Team_id>(),
+								std::vector<mue::Team_id> {0,1,6,8,0,0,0,0,0});
+
+			TS_ASSERT_EQUALS(calc.guest_distance(round_data, 3, mue::Guest_tuple_generator::GuestPair(1, 2)), 1+2);
+			TS_ASSERT_EQUALS(calc.guest_distance(round_data, 0, mue::Guest_tuple_generator::GuestPair(6, 7)), 0+0);
+		}
+
 };
 
 
