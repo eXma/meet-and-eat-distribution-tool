ae7fa0a024559457e70e81fab6c9f115c15e122f
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-09-02 17:45:12 +0200

    Add a Calculation state container
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- libmue/mue_algorithm.cpp
+++ libmue/mue_algorithm.cpp
@@ -1,6 +1,23 @@
 #include "mue_algorithm.h"
 
-std::string mue::peng()
+mue::Calculation::Calculation(unsigned int teamcount, Distance_matrix const &distance_matrix)
+:
+	_teamcount(teamcount),
+	_teams_per_round(teamcount / 3),
+	_distance_matrix(distance_matrix),
+	_team_round_random(0, _teams_per_round -1),
+	_random_generator(std::random_device()())
 {
-	return "fooBar";
+	BOOST_ASSERT(_teamcount <= MAX_TEAMS);
+	BOOST_ASSERT(_teamcount / 3 == float(_teamcount / 3));
+}
+
+
+mue::Distance mue::Calculation::dummy_distance(Team_id host, Guest_tuple_generator::GuestPair const &guests)
+{
+	if (_is_round_host(guests.first, SECOND))
+		return _distance_matrix.lookup(host, guests.first);
+	if (_is_round_host(guests.second, SECOND))
+		return _distance_matrix.lookup(host, guests.second);
+	return _distance_matrix.lookup(host, _random_host(SECOND)) * 2;
 }
--- libmue/mue_algorithm.h
+++ libmue/mue_algorithm.h
@@ -1,11 +1,52 @@
 #ifndef MUE_ALGORITHM_H
 #define MUE_ALGORITHM_H
 
-#include <string>
+#include <random>
+
+
+#include <boost/assert.hpp>
+
+#include "guest_tuple_iterator.h"
+#include "distances.h"
+#include "config.h"
 
 
 namespace mue {
-	std::string peng();
+
+	class Calculation
+	{
+		private:
+			enum Round {
+				FIRST = 0,
+				SECOND = 1,
+				THIRD = 2
+			};
+
+
+			unsigned int _teamcount;
+			unsigned int _teams_per_round;
+			Distance_matrix const _distance_matrix;
+			std::uniform_int_distribution<Team_id> _team_round_random;
+			std::mt19937 _random_generator;
+
+
+			bool _is_round_host(Team_id team, Round round)
+			{
+				return     (_teams_per_round * round       <= team)
+					&& (_teams_per_round * (round + 1) >  team);
+			}
+
+			Team_id _random_host(Round round)
+			{
+				return _team_round_random(_random_generator) + (_teams_per_round * round);
+			}
+		
+		public:
+			Calculation(unsigned int teamcount, Distance_matrix const &distance_matrix);
+
+			Distance dummy_distance(Team_id host, Guest_tuple_generator::GuestPair const &guests);
+	};
+
 }
 
 #endif
