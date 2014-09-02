f8dc376c2686ca63d5c1108fe48623f22621cb88
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-09-02 22:24:17 +0200

    Add some real calculation functions to Calculation
    It has now two data-containers: one containg the round specific data and
    one containing the iteration specific data. These containers are
    structs because we want fast data access.
    
    The random stuff is declared mutable because requesting random numbers
    is not side-effect-free. But this should not matter because we need it
    only to have some sort of random choicing at the beginning. There is no
    need for a pefect distributed random. This allows us to declare a lot of
    methods const
    
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- libmue/mue_algorithm.cpp
+++ libmue/mue_algorithm.cpp
@@ -1,3 +1,6 @@
+#include <limits>
+#include <iostream>
+
 #include "mue_algorithm.h"
 
 mue::Calculation::Calculation(unsigned int teamcount, Distance_matrix const &distance_matrix)
@@ -6,18 +9,46 @@ mue::Calculation::Calculation(unsigned int teamcount, Distance_matrix const &dis
 	_teams_per_round(teamcount / 3),
 	_distance_matrix(distance_matrix),
 	_team_round_random(0, _teams_per_round -1),
-	_random_generator(std::random_device()())
+	_random_generator(std::random_device()()),
+	_best_distance(std::numeric_limits<float>::max())
 {
 	BOOST_ASSERT(_teamcount <= MAX_TEAMS);
 	BOOST_ASSERT(_teamcount / 3 == float(_teamcount / 3));
 }
 
 
-mue::Distance mue::Calculation::dummy_distance(Team_id host, Guest_tuple_generator::GuestPair const &guests)
+mue::Distance mue::Calculation::dummy_distance(Team_id host, Guest_tuple_generator::GuestPair const &guests) const
 {
 	if (_is_round_host(guests.first, SECOND))
 		return _distance_matrix.lookup(host, guests.first);
 	if (_is_round_host(guests.second, SECOND))
 		return _distance_matrix.lookup(host, guests.second);
 	return _distance_matrix.lookup(host, _random_host(SECOND)) * 2;
+}
+
+
+mue::Distance mue::Calculation::guest_distance(Round_data const &round_data, Team_id host, Guest_tuple_generator::GuestPair const &guests) const
+{
+	return _distance_matrix.lookup(round_data.prev_host(guests.first), host) +
+	       _distance_matrix.lookup(round_data.prev_host(guests.second), host);
+	      
+}
+
+std::vector<mue::Calculation::Guest_candidate> mue::Calculation::determine_guest_candidates(Round_data const &round_data, Iteration_data const &iteration_data, Team_id current_host) const
+{
+	std::vector<Guest_candidate> candidates;
+	Guest_tuple_generator generator(round_data.guests, iteration_data.used_guests);
+	for (Guest_tuple_generator::GuestPair const &guests : generator) {
+		if (! iteration_data.seen_table.seen(current_host, guests.first, guests.second)) {
+			if (! round_data.first_round()) {
+				float distance = iteration_data.distance + guest_distance(round_data, current_host, guests);
+				if (distance < _best_distance)
+					candidates.emplace_back(iteration_data.distance + guest_distance(round_data, current_host, guests), guests);
+			} else {
+				candidates.emplace_back(iteration_data.distance + dummy_distance(current_host, guests), guests);
+			}
+		}
+	}
+	std::sort(candidates.begin(), candidates.end(), [](Guest_candidate const &a, Guest_candidate const &b) { return a.first < b.first; });
+	return candidates;
 }
--- libmue/mue_algorithm.h
+++ libmue/mue_algorithm.h
@@ -9,13 +9,16 @@
 #include "guest_tuple_iterator.h"
 #include "distances.h"
 #include "config.h"
+#include "seen_table.h"
 
 
 namespace mue {
 
+
+
 	class Calculation
 	{
-		private:
+		public:
 			enum Round {
 				FIRST = 0,
 				SECOND = 1,
@@ -23,20 +26,97 @@ namespace mue {
 			};
 
 
+			struct Round_data
+			{
+				Calculation::Round   const round;
+				std::vector<Team_id> const hosts;
+				std::vector<Team_id> const guests;
+				std::vector<Team_id> const prev_stations;
+
+				bool first_round() const { return round == Calculation::FIRST; }
+				Team_id prev_host(Team_id team) const { return prev_stations[team]; }
+
+				Round_data(Calculation::Round round,
+					   std::vector<Team_id> const &hosts,
+					   std::vector<Team_id> const &guests,
+					   std::vector<Team_id> const &prev_stations)
+				:
+					round(round),
+					hosts(hosts),
+					guests(guests),
+					prev_stations(prev_stations)
+				{ }
+
+				Round_data()
+				:
+					round(FIRST),
+					hosts(),
+					guests(),
+					prev_stations()
+				{}
+			};
+
+
+			struct Iteration_data
+			{
+				Distance                    distance;
+				std::unordered_set<Team_id> used_guests;
+				std::vector<Team_id>        round_station;
+				Seen_table                  seen_table;
+
+				void set_station(Team_id host, Team_id guest1, Team_id guest2)
+				{
+					round_station[host] = host;
+					round_station[guest1] = host;
+					round_station[guest2] = host;
+					used_guests.insert(guest1);
+					used_guests.insert(guest2);
+				}
+
+				Iteration_data(unsigned int teamcount)
+				:
+					distance(0),
+					used_guests(),
+					round_station(teamcount),
+					seen_table(teamcount)
+				{ }
+
+				Iteration_data(unsigned int teamcount, Distance distance)
+				:
+					distance(distance),
+					used_guests(),
+					round_station(teamcount),
+					seen_table(teamcount)
+				{ }
+
+				Iteration_data(Iteration_data const &other)
+				:
+					distance(other.distance),
+					used_guests(other.used_guests),
+					round_station(other.round_station),
+					seen_table(other.seen_table.clone())
+				{ }
+			};
+
+			typedef std::pair<Distance, Guest_tuple_generator::GuestPair> Guest_candidate;
+
+		private:
+
 			unsigned int _teamcount;
 			unsigned int _teams_per_round;
 			Distance_matrix const _distance_matrix;
-			std::uniform_int_distribution<Team_id> _team_round_random;
-			std::mt19937 _random_generator;
+			mutable std::uniform_int_distribution<Team_id> _team_round_random;
+			mutable std::mt19937 _random_generator;
+			Distance _best_distance;
 
 
-			bool _is_round_host(Team_id team, Round round)
+			bool _is_round_host(Team_id team, Round round) const
 			{
 				return     (_teams_per_round * round       <= team)
 					&& (_teams_per_round * (round + 1) >  team);
 			}
 
-			Team_id _random_host(Round round)
+			Team_id _random_host(Round round) const
 			{
 				return _team_round_random(_random_generator) + (_teams_per_round * round);
 			}
@@ -44,8 +124,16 @@ namespace mue {
 		public:
 			Calculation(unsigned int teamcount, Distance_matrix const &distance_matrix);
 
-			Distance dummy_distance(Team_id host, Guest_tuple_generator::GuestPair const &guests);
+			unsigned int teamcount() { return _teamcount; }
+
+			Distance dummy_distance(Team_id host, Guest_tuple_generator::GuestPair const &guests) const;
+
+			Distance guest_distance(Round_data const &round_data, Team_id host, Guest_tuple_generator::GuestPair const &guests) const;
+
+			std::vector<Guest_candidate> determine_guest_candidates(Round_data const &round_data, Iteration_data const &iteration_data, Team_id current_host) const;
+
 	};
+
 
 }
 
