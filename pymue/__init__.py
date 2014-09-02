e8e944d3b3ced0b79dff636324593fc862b77b3c
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-09-02 18:01:41 +0200

    Fix cleanup fallout
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- pymue/__init__.py
+++ pymue/__init__.py
@@ -1,4 +1,4 @@
-from _pymue import peng, Team, DistanceMatrix, SeenTable, GuestTupleGenerator, GuestPair, Calculation
+from _pymue import Team, DistanceMatrix, SeenTable, GuestTupleGenerator, GuestPair, Calculation
 
 def pair_pprint(pair):
     return "(%s, %s)" % (pair.first, pair.second)
