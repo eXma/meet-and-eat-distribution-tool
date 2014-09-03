e5b60a6d1ba49b3819cfa5cf6acda0525c3b5cf5
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-09-03 02:51:32 +0200

    Replace a lot of python code with c++ bindings
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- mue/integration_test.py
+++ mue/integration_test.py
@@ -68,23 +68,12 @@ def round_guests(hosts):
     return [team for team in teams if team not in hosts]
 
 
-def add_meeting(host, guests, seen_table, meetings, stations):
-    tbl = seen_table.clone()
-    tbl.add_meeting(host, guests.first, guests.second)
+def add_meeting(host, guests, iteration_data, meetings):
+    iteration_data.set_station(host, guests.first, guests.second)
 
     meeting = {host, guests.first, guests.second}
     meetings.append(meeting)
-    for team in [host, guests.first, guests.second]:
-        stations[team] = host
-    return tbl
 
-
-def distance(current_distance, prev_stations, teams, host):
-    for team in teams:
-        prev_host = prev_stations[team]
-        dist = distance_matrix.lookup(prev_host, host)
-        current_distance += dist
-    return current_distance
 
 
 second_hosts_list = round_hosts(1)
@@ -95,35 +84,10 @@ i = 0
 best_distance = sys.float_info.max
 best_plan = None
 
-def guestsort(a, b):
-    if a[0] < b[0]:
-        return -1
-    elif a[0] == b[0]:
-        return 0
-    else:
-        return 1
 
 
-def determine_guests(current_guests, used_guests, actual_host, seen_table, current_round, current_distance, last_stations):
-    possible_guests = []
-    round_data = pymue.RoundData(current_round, [], current_guests, [last_stations[i] for i in sorted(last_stations.keys())])
-    for guests in pymue.GuestTupleGenerator(current_guests, used_guests):
-        if not seen_table.seen(actual_host, guests.first, guests.second):
-            if current_round > 0:
-                actual_distance = calculation.guest_distance(round_data, actual_host, guests) + current_distance
-                if actual_distance >= best_distance:
-                    continue
-            else:
-                actual_distance = calculation.dummy_distance(actual_host, guests)
-            possible_guests.append((actual_distance, guests))
-
-
-    possible_guests.sort(guestsort)
-    return possible_guests;
-
-
-def deploy_host(host_idx, current_hosts, current_round, current_guests, used_guests, seen_table, meetings_list,
-                stations_list, current_distance):
+def deploy_host(host_idx, current_hosts, current_round, current_guests,  meetings_list,
+                iteration_data, round_data):
     """
     :param seen_table: dict of set
     """
@@ -132,57 +96,59 @@ def deploy_host(host_idx, current_hosts, current_round, current_guests, used_gue
             new_round = current_round + 1
             new_hosts = round_hosts(new_round)
             new_guests = round_guests(new_hosts)
-            return deploy_host(0, new_hosts, new_round, new_guests, list(), seen_table, meetings_list, stations_list,
-                               current_distance)
+            round_data = pymue.RoundData(new_round, new_hosts, new_guests, [x for x in iteration_data.round_station])
+            iteration_data.clear_round_data()
+            return deploy_host(0, new_hosts, new_round, new_guests, meetings_list,
+                               iteration_data, round_data)
         else:
             global i, best_distance, best_plan
             i += 1
             #print "ENDPOINT", meetings_list
-            if current_distance < best_distance:
-                print "new best (%i)" % i, current_distance, meetings_list
-                best_distance = current_distance
+            if iteration_data.distance < best_distance:
+                print "new best (%i)" % i, iteration_data.distance, meetings_list
+                best_distance = iteration_data.distance
+                calculation.update_best(best_distance)
                 best_plan = meetings_list
             return
 
     tests = cnt_hosts * 3
     actual_host = current_hosts[host_idx]
     if current_round > 0:
-        current_distance = distance(current_distance, stations_list[current_round - 1],
-                                    [actual_host], actual_host)
-        if current_distance >= best_distance:
-            #print "skip combination in round", current_round, "for host dist", current_distance, "best", best_distance
+        iteration_data.distance = calculation.host_distance(round_data, actual_host) + iteration_data.distance
+        if iteration_data.distance >= best_distance:
             return
 
-    possible_guests = determine_guests(current_guests, used_guests, actual_host, seen_table, current_round, current_distance, stations_list[current_round - 1])
+    possible_guests = calculation.determine_guest_candidates(round_data, iteration_data, actual_host)
 
 
     if current_round > 0:
         tests = cnt_hosts / 3
 
-    for (actual_distance, guests) in possible_guests[:tests]:
+    for candidate in possible_guests[:tests]:
         if current_round == 0:
-            actual_distance = current_distance
+            actual_distance = iteration_data.distance
+        else:
+            actual_distance = candidate.distance
+        guests = candidate.guests
 
+        new_iteration_data = pymue.IterationData(iteration_data)
+        new_iteration_data.distance = actual_distance
         actual_meetings = list(meetings_list) # XXX copy.deepcopy(meetings)
         meetings = list(actual_meetings[current_round])
         actual_meetings[current_round] = meetings
 
-        actual_stations = list(stations_list)
-        stations = actual_stations[current_round].copy()
-        actual_stations[current_round] = stations
-
-        actual_seen = add_meeting(actual_host, guests, seen_table, meetings, stations)
-        actual_used = list(used_guests)
-        actual_used.extend([guests.first, guests.second])
-        deploy_host(host_idx + 1, current_hosts, current_round, current_guests, actual_used, actual_seen,
-                    actual_meetings, actual_stations, actual_distance)
+        add_meeting(actual_host, guests, new_iteration_data, meetings)
+        deploy_host(host_idx + 1, current_hosts, current_round, current_guests,
+                    actual_meetings, new_iteration_data, round_data)
 
 
 def test():
     current_hosts = round_hosts(0)
     current_guests = round_guests(current_hosts)
 
-    deploy_host(0, current_hosts, 0, current_guests, [], seen, [[], [], []], [{}, {}, {}], 0)
+    round_data = pymue.RoundData(0, current_hosts, current_guests, [])
+    iteration_data = pymue.IterationData(cnt)
+    deploy_host(0, current_hosts, 0, current_guests, [[], [], []], iteration_data, round_data)
     print ""
     print "======best plan======"
     print "1st round:", best_plan[0]
