36a9564baf47e406ac726b719b12196c13931f18
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-09-03 21:47:30 +0200

    Move away from Calculation_wrapper
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- pymue/pymue.cpp
+++ pymue/pymue.cpp
@@ -7,7 +7,6 @@
 
 #include "seen_table_wrapper.h"
 #include "guest_tuple_iterator_wrapper.h"
-#include "calculation_wrapper.h"
 
 #include <vector>
 
@@ -39,10 +38,11 @@ BOOST_PYTHON_MODULE (_pymue)
 	class_<pymue::Guest_tuple_generator_wrapper>("GuestTupleGenerator", init<list, list>())
 		.def("__iter__", iterator<pymue::Guest_tuple_generator_wrapper>());
 
-	class_<pymue::Calculation_wrapper::Round_data_wrapper>("RoundData", init<unsigned int, list, list, list>())
-		.def_readonly("round", &pymue::Calculation_wrapper::Round_data_wrapper::round)
-		.def("first_round", &pymue::Calculation_wrapper::Round_data_wrapper::first_round)
-		.def("prev_host", &pymue::Calculation_wrapper::Round_data_wrapper::prev_host);
+	class_<mue::Calculation::Round_data>("RoundData", init<>())
+		.def_readonly("round", &mue::Calculation::Round_data::round)
+		.def_readonly("hosts", &mue::Calculation::Round_data::hosts)
+		.def("first_round", &mue::Calculation::Round_data::first_round)
+		.def("prev_host", &mue::Calculation::Round_data::prev_host);
 
 	class_<mue::Calculation::Iteration_data>("IterationData", init<unsigned int>())
 		.def(init<mue::Calculation::Iteration_data const>())
@@ -60,11 +60,13 @@ BOOST_PYTHON_MODULE (_pymue)
 	class_<std::vector<mue::Calculation::Guest_candidate> >("GuestCandidateVector")
 		.def(vector_indexing_suite<std::vector<mue::Calculation::Guest_candidate> >());
 
-	class_<pymue::Calculation_wrapper>("Calculation", init<unsigned int, mue::Distance_matrix>())
+	class_<mue::Calculation>("Calculation", init<unsigned int, mue::Distance_matrix>())
 		.def("update_best", &mue::Calculation::update_best)
+		.def("initial_round_data", &mue::Calculation::initial_round_data)
+		.def("next_round_data", &mue::Calculation::next_round_data)
 		.def("dummy_distance", &mue::Calculation::dummy_distance)
-		.def("guest_distance", &pymue::Calculation_wrapper::guest_distance)
-		.def("host_distance", &pymue::Calculation_wrapper::host_distance)
-		.def("determine_guest_candidates", &pymue::Calculation_wrapper::determine_guest_candidates);
+		.def("guest_distance", &mue::Calculation::guest_distance)
+		.def("host_distance", &mue::Calculation::host_distance)
+		.def("determine_guest_candidates", &mue::Calculation::determine_guest_candidates);
 
 }
