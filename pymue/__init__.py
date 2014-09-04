283b024b83a9bbbf27b7cc1936e3022987ca94d7
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-09-04 03:43:38 +0200

    Adjust python bindings
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- pymue/__init__.py
+++ pymue/__init__.py
@@ -1,6 +1,1 @@
-from _pymue import Team, DistanceMatrix, GuestPair, Calculation, RoundData, IterationData, GuestCandidate, Round
-
-def pair_pprint(pair):
-    return "(%s, %s)" % (pair.first, pair.second)
-
-GuestPair.__repr__ = pair_pprint
+from _pymue import DistanceMatrix, Calculation, Round
--- pymue/pymue.cpp
+++ pymue/pymue.cpp
@@ -12,9 +12,6 @@ BOOST_PYTHON_MODULE (_pymue)
 {
 	using namespace boost::python;
 
-	class_<mue::Team>("Team", init<int>())
-		.def("id", &mue::Team::id);
-
 	class_<mue::Distance_matrix>("DistanceMatrix", init<unsigned int>())
 		.def("set_cost", &mue::Distance_matrix::set_cost)
 		.def("lookup", &mue::Distance_matrix::lookup);
@@ -22,42 +19,10 @@ BOOST_PYTHON_MODULE (_pymue)
 	class_<std::vector<mue::Team_id> >("TeamVector")
 		.def(vector_indexing_suite<std::vector<mue::Team_id> >());
 
-	class_<mue::Guest_tuple_generator::GuestPair>("GuestPair", init<mue::Team_id, mue::Team_id>())
-		.def_readonly("first",  &mue::Guest_tuple_generator::GuestPair::first)
-		.def_readonly("second", &mue::Guest_tuple_generator::GuestPair::second);
-
-	class_<mue::Calculation::Round_data>("RoundData", init<>())
-		.def_readonly("round", &mue::Calculation::Round_data::round)
-		.def_readonly("hosts", &mue::Calculation::Round_data::hosts)
-		.def("first_round", &mue::Calculation::Round_data::first_round)
-		.def("prev_host", &mue::Calculation::Round_data::prev_host);
-
-	class_<mue::Calculation::Iteration_data>("IterationData", init<unsigned int>())
-		.def(init<mue::Calculation::Iteration_data const>())
-		.def("seen", &mue::Calculation::Iteration_data::seen)
-		.def_readonly("round_station", &mue::Calculation::Iteration_data::round_station)
-		.def_readonly("used_guests", &mue::Calculation::Iteration_data::used_guests)
-		.def_readwrite("distance", &mue::Calculation::Iteration_data::distance)
-		.def("set_station", &mue::Calculation::Iteration_data::set_station)
-		.def("next_iteration", &mue::Calculation::Iteration_data::next_iteration)
-		.def("clear_round_data", &mue::Calculation::Iteration_data::clear_round_data);
-
-	class_<mue::Calculation::Guest_candidate>("GuestCandidate", init<mue::Distance, mue::Guest_tuple_generator::GuestPair>())
-		.def_readonly("distance", &mue::Calculation::Guest_candidate::distance)
-		.def_readonly("guests", &mue::Calculation::Guest_candidate::guests);
-
-	class_<std::vector<mue::Calculation::Guest_candidate> >("GuestCandidateVector")
-		.def(vector_indexing_suite<std::vector<mue::Calculation::Guest_candidate> >());
-
 	class_<mue::Calculation>("Calculation", init<unsigned int, mue::Distance_matrix, mue::Distance>())
-		.def("update_best", &mue::Calculation::update_best)
-		.def("initial_round_data", &mue::Calculation::initial_round_data)
-		.def("next_round_data", &mue::Calculation::next_round_data)
-		.def("round_stations", &mue::Calculation::round_stations)
-		.def("dummy_distance", &mue::Calculation::dummy_distance)
-		.def("guest_distance", &mue::Calculation::guest_distance)
-		.def("host_distance", &mue::Calculation::host_distance)
-		.def("determine_guest_candidates", &mue::Calculation::determine_guest_candidates);
+		.def("calculate_distribution", &mue::Calculation::calculate_distribution)
+		.def("solutions", &mue::Calculation::solutions)
+		.def("round_stations", &mue::Calculation::round_stations);
 
 	enum_<mue::Calculation::Round>("Round")
 		.value("FIRST", mue::Calculation::FIRST)
