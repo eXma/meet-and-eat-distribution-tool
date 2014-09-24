6302fd9baf6c465921bdada8cd63dee236546f6b
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-09-24 16:23:40 +0200

    Add unittests for the forecast calculation
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- libmue/tests/test_distances.h
+++ libmue/tests/test_distances.h
@@ -50,6 +50,25 @@ class TestDistances : public CxxTest::TestSuite
 
 		}
 
+		void testDistanceForecast(void)
+		{
+			mue::Distance_matrix distances(MAX_TEAMS);
+
+			for (int i = 0; i < MAX_TEAMS; ++i) {
+				for (int j = i; j < MAX_TEAMS; ++j) {
+					distances.set_cost(mue::Team(i), mue::Team(j), 1);
+				}
+			}
+
+			mue::Distance_forecast forecast(distances);
+
+			unsigned hosts = MAX_TEAMS / 3;
+			for (unsigned i = 0; i < hosts; ++i) {
+				TS_ASSERT_EQUALS(forecast.second_move(i), hosts - i - 1);
+				TS_ASSERT_EQUALS(forecast.first_move(i), 2 * hosts - i - 1);
+			}
+		}
+
 };
 
 #endif
