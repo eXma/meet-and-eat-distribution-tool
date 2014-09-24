dd10fe15618820a7ddd5cd17248e22f2864b0316
Jan Losinski <losinski@wh2.tu-dresden.de>
2014-09-24 16:22:15 +0200

    Put forecast calculation into a seperate class
    The forecasts for the combination tesps are now precalculated from the
    Distance_matrix and can be simply queried
    
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- libmue/distance.cpp
+++ libmue/distance.cpp
@@ -1,5 +1,6 @@
 #include <boost/assert.hpp>
 #include <limits>
+#include <algorithm>
 
 
 #include "distances.h"
@@ -26,4 +27,52 @@ void mue::Distance_matrix::set_cost(Team_id const source, Team_id const destinat
 
 	if (cost < _min_cost)
 		_min_cost = cost;
+}
+
+
+std::vector<mue::Distance>
+mue::Distance_forecast::_min_costs(Distance_matrix const &distances, unsigned hostcount,
+		                   unsigned multiply)
+{
+	std::vector<Distance> first_tmp;
+	for (unsigned src_index = 0; src_index < hostcount; ++src_index) {
+		for (unsigned dst_index = 0; dst_index < hostcount; ++dst_index) {
+			first_tmp.push_back(distances.lookup(src_index + multiply * hostcount,
+							     dst_index + (multiply + 1) * hostcount));
+		}
+	}
+
+	std::sort(first_tmp.begin(), first_tmp.end(),
+			[] (Distance const &a, Distance const &b) { return a > b; });
+
+	BOOST_ASSERT(first_tmp.size() >= hostcount);
+
+	return std::vector<Distance>(first_tmp.end() - hostcount, first_tmp.end());
+}
+
+
+std::vector<mue::Distance>
+mue::Distance_forecast::_fill_costs(Distance_matrix const &distances, unsigned hostcount,
+		                    unsigned multiply, Distance const &offset)
+{
+	std::vector<Distance> min_costs(_min_costs(distances, hostcount, multiply));
+
+	std::vector<Distance> accumulated;
+	for (auto i = min_costs.begin(); i != min_costs.end(); ++i) {
+		accumulated.push_back(std::accumulate(i, min_costs.end(), offset,
+				      [] (Distance const &a, Distance const &b) { return a + b; }));
+	}
+	accumulated.push_back(offset);
+
+	BOOST_ASSERT(accumulated.size() == hostcount + 1);
+	return accumulated;
+}
+
+
+mue::Distance_forecast::Distance_forecast(Distance_matrix const &distances)
+:
+	_hostcount(distances.teamcount() / 3),
+	_second_costs(_fill_costs(distances, _hostcount, 1, 0)),
+	_first_costs(_fill_costs(distances, _hostcount, 0, _second_costs[0]))
+{
 }
--- libmue/distances.h
+++ libmue/distances.h
@@ -1,8 +1,9 @@
 #ifndef MUE_DISTANCES_H
 #define MUE_DISTANCES_H
 
-
 #include <vector>
+
+#include <boost/assert.hpp>
 
 
 #include <teams.h>
@@ -11,6 +12,7 @@
 namespace mue {
 
 	typedef float Distance;
+
 
 	class Distance_matrix
 	{
@@ -32,6 +34,39 @@ namespace mue {
 			int teamcount() const { return _teamcount; }
 
 			Distance min_cost() const { return _min_cost; }
+	};
+
+
+	class Distance_forecast
+	{
+		private:
+			unsigned _hostcount;
+			std::vector<Distance> _second_costs;
+			std::vector<Distance> _first_costs;
+
+
+			static std::vector<Distance>
+			_min_costs(Distance_matrix const &distances, unsigned hostcount,
+				   unsigned multiply);
+
+			static std::vector<Distance>
+			_fill_costs(Distance_matrix const &distances, unsigned hostcount,
+				    unsigned multiply, Distance const &offset);
+
+		public:
+			Distance_forecast(Distance_matrix const &distances);
+
+			Distance first_move(size_t host_idx) const
+			{
+				BOOST_ASSERT(host_idx + 1 < _first_costs.size());
+				return _first_costs[host_idx + 1];
+			}
+
+			Distance second_move(size_t host_idx) const
+			{
+				BOOST_ASSERT(host_idx + 1 < _second_costs.size());
+				return _second_costs[host_idx + 1];
+			}
 	};
 }
 
