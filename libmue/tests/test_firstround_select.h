eb0d8c0e50862e4b1a7e74207a33238f27bf6d3f
Jan Losinski <losinski@wh2.tu-dresden.de>
2015-05-06 00:22:57 +0200

    update reference data for TestFirstroundTeamSelection
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- libmue/tests/test_firstround_select.h
+++ libmue/tests/test_firstround_select.h
@@ -7,7 +7,7 @@
 #include "config.h"
 #include "guest_tuple_iterator.h"
 
-
+#include <iostream>
 class TestFirstroundTeamSelection : public CxxTest::TestSuite
 {
 	private:
@@ -60,11 +60,40 @@ class TestFirstroundTeamSelection : public CxxTest::TestSuite
 
 			using guests = mue::Guest_tuple_generator::GuestPair;
 
-			std::vector<guests> reference = { guests(4, 8), guests(4, 9), guests(4, 10),
-							  guests(8, 9), guests(8, 10),
-							  guests(9, 10)};
+			std::vector<guests> reference = {guests(4, 8),
+							 guests(4, 9),
+							 guests(4, 10),
+							 guests(4, 5),
+							 guests(4, 6),
+							 guests(4, 7),
+							 guests(4, 11),
+							 guests(8, 9),
+							 guests(8, 10),
+							 guests(8, 5),
+							 guests(8, 6),
+							 guests(8, 7),
+							 guests(8, 11),
+							 guests(9, 10),
+							 guests(9, 5),
+							 guests(9, 6),
+							 guests(9, 7),
+							 guests(9, 11),
+							 guests(10, 5),
+							 guests(10, 6),
+							 guests(10, 7),
+							 guests(10, 11),
+							 guests(5, 6),
+							 guests(5, 7),
+							 guests(5, 11),
+							 guests(6, 7),
+							 guests(6, 11),
+							 guests(7, 11)};
+
+
+
 			auto it = reference.begin();
 			for (guests const &g : generator) {
+				std::cout << "guests(" << (int) g.first << ", "<< (int) g.second << ")"<<std::endl;
 				TS_ASSERT(it != reference.end());
 				TS_ASSERT_EQUALS(it->first, g.first);
 				TS_ASSERT_EQUALS(it->second, g.second);
