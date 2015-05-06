79ae9cae61899918e817578327ff78ce94a16f52
Jan Losinski <losinski@wh2.tu-dresden.de>
2015-05-06 02:05:27 +0200

    Simplify Seen_table memory handling
    As pointed out by @devreal a lot of time is lost by dynamic memory
    management for the copy-on-write mechanism of the Seen_table. So this
    commit simply drop this and use full copys of the data tables.
    
    From am memory usage point of view this should not really matter,
    because we remove the array of pointers to the table entries. A table
    entry itself is obly a std::bitset of MAX_TEAMS - which takes up to
    eight bytes. So we save eight bytes by not storing the pointers and use
    another eight by having the whole table.
    
    The table is mmecpy-ed - so we save the element copy-construction.
    
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- libmue/seen_table.h
+++ libmue/seen_table.h
@@ -13,32 +13,20 @@ class Seen_table
 {
 
 	private:
-		struct Seen_data {
-			typedef std::bitset<MAX_TEAMS> Seen_bitset;
+		typedef std::bitset<MAX_TEAMS> Seen_bitset;
 
-			int const generation;
-			Seen_bitset bitset;
-
-			Seen_data(int generation) : generation(generation), bitset() { }
-			Seen_data(int generation, Seen_bitset const &bitset) : generation(generation), bitset(bitset) { }
-			Seen_data(Seen_data const &other) : generation(other.generation + 1), bitset(other.bitset) { }
-		};
-
-		static std::vector<Seen_data::Seen_bitset> _id_bitsets;
+		static std::vector<Seen_bitset> _id_bitsets;
 
 		int const _generation;
 		int const _max_teams;
 
-		std::vector<Seen_data *> _table;
-		std::list<Seen_data *> _allocated_lines;
+		Seen_bitset _table[MAX_TEAMS];
 
 		Seen_table(Seen_table const &old, int new_generation) noexcept;
 
 		inline void _update_table(Team_id table_idx, Team_id team_a, Team_id team_b) noexcept
 		{
-			Seen_data * new_data = new Seen_data(generation(), _table[table_idx]->bitset | (_id_bitsets[team_a] | _id_bitsets[team_b]));
-			_allocated_lines.push_back(new_data);
-			_table[table_idx] = new_data;
+			_table[table_idx] |= (_id_bitsets[team_a] | _id_bitsets[team_b]);
 		}
 
 	public:
@@ -56,15 +44,13 @@ class Seen_table
 
 		bool seen(Team_id id_1, Team_id id_2, Team_id id_3) const noexcept
 		{
-			return (_table[id_1]->bitset & (_id_bitsets[id_2] | _id_bitsets[id_3])).any()
-				|| (_table[id_2]->bitset & _id_bitsets[id_3]).any();
+			return (_table[id_1] & (_id_bitsets[id_2] | _id_bitsets[id_3])).any()
+				|| (_table[id_2] & _id_bitsets[id_3]).any();
 		}
 
 		void add_meeting(Team_id id_1, Team_id id_2, Team_id id_3) noexcept;
 
 		Seen_table(int max_teams) noexcept;
-
-		~Seen_table();
 };
 
 }
